239dcbe8d2c5185430b762496195706453890246
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-05-21 02:56:59 +0900

    カレンダーページの見た目と遷移の追加

---
+++ lib/calendar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarPage extends ConsumerStatefulWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends ConsumerState<CalendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('カレンダー')),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2010, 1, 1),
+        lastDay: DateTime.utc(2030, 1, 1),
+        focusedDay: DateTime.now(),
+      ),
+    );
+  }
+}
